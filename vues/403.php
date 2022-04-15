3afee9dbc7f93ffdb74e876d2e9d7e4a67b85a47
axllblc <59664317+axllblc@users.noreply.github.com>
2022-04-15 18:19:06 +0200

    Refuser l'accès au fichier README.md

---
+++ vues/403.php
@@ -0,0 +1,17 @@
+<?php
+
+/* ⚠️ Erreur 403 */
+
+?>
+
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Erreur 403</title>
+</head>
+<body>
+  <strong>Erreur 403</strong> : Accès refusé.
+</body>
+</html>
