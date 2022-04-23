a30f6c71cda17c0758cad9f3e3f6510afcb9832f
axllblc <59664317+axllblc@users.noreply.github.com>
2022-04-23 22:43:08 +0200

    Documentation de `db.inc.php`

--- traitements/db.inc.php
+++ traitements/db.inc.php
@@ -1,13 +1,15 @@
 <?php
 
+/* Connexion à la base de données */
+
 // Ne pas lever d'exception en cas d'erreur (comportement par défaut de PHP < 8.1)
 mysqli_report(MYSQLI_REPORT_OFF);
 
 /**
- * Se connecter à la base de données. Retourne un objet mysqli
+ * Se connecter à la base de données. Retourne un objet mysqli permettant de faire le lien avec la base de données.
+ * @return bool|mysqli
  */
-function dbConnect ()
-{
+function dbConnect (): bool|mysqli {
     $link = mysqli_connect('localhost', 'Avocaba', 'IPx@tVqM46!-zPyI', 'avocaba');
     checkError($link, $link, 'Erreur de connexion à la base de données.');
 
@@ -20,8 +22,11 @@ function dbConnect ()
 /**
  * Vérifier la présence d'erreurs.
  * Si $data vaut false, la connexion à la base de données est fermée et le script est interrompu.
+ * @param mixed $data Donnée à évaluer
+ * @param mysqli $link Objet mysqli permettant de faire le lien avec la base de données
+ * @param string $message Message à afficher en cas d'erreur
  */
-function checkError ($data, $link, $message = 'Une erreur s\'est produite.') {
+function checkError (mixed $data, mysqli $link, string $message = 'Une erreur s\'est produite.'): void {
     if (!$data) {
         if ($link) $link->close();
         exit($message);
