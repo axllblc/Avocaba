bdc0dbfa78d7af3b55546e59459d0429bdb0416c
axllblc <59664317+axllblc@users.noreply.github.com>
2022-05-02 15:45:36 +0200

    TODOs pour les pages d'authentification

--- vues/espace-client/signin.php
+++ vues/espace-client/signin.php
@@ -38,25 +38,38 @@ if ( !empty($_POST['email']) and !empty($_POST['motdepasse']) ) {
 <?php htmlHead('Se connecter – Avocaba'); ?>
 
 <body>
+  <!-- TODO : éviter d'imbriquer plusieurs éléments lorsque ce n'est pas nécessaire : ici, on a juste besoin de
+              conserver l'élément <form> ; on peut supprimer ce qui est autour -->
   <main class="connexion">
     <div id="connexion_formulaire">
-      <form class="connexion_connecter" action="signin.php" method="POST">
+      <form class="connexion_connecter" action="" method="POST">
         <button class="nav__btn-retour" onclick="history.back();" title="Revenir à la page précédente">Retour</button>
         <h1 id="connexion__titre">Connexion</h1>
+
         <label for="email">Adresse email</label><br>
         <input type="email" size="40" name="email" id="email"
-               pattern="^[a-zA-Z1-9-\.]+@[a-zA-Z1-9-]+\.[a-zA-Z]{2,6}$"
+               pattern="^[a-zA-Z1-9-.]+@[a-zA-Z1-9-]+\.[a-zA-Z]{2,6}$"
                value="<?php if (isset($_POST['email'])) echo $_POST['email']; ?>"
                required><br>
         <label for="motdepasse">Mot de passe</label><br>
         <input type="password" size="40" name="motdepasse" id="motdepasse" minlength="8" maxlength="16" pattern="([0-9a-zA-Z._#-]){8,16}" required><br>
         <input type="checkbox" name="sesouvenirdemoi" id="check">
         <label for="check">Se souvenir de moi</label><br><br>
+
+        <!-- TODO : Supprimer le <span> autour de la balise <a>
+                    Associer l'élément <a> à une classe partagée par tous les boutons du même type présents dans les
+                    pages d'authentification (elle est à définir dans une/les feuille(s) de style).
+                    Déplacer le CSS de l'attribut "style" dans le(s) fichier(s) CSS correspondant(s) -->
         <span id="connexion_bouton_mdp"><a style="text-decoration:none" href="reset-password.php" title="Cliquez ici pour réinitialiser votre mot de passe">J'ai oublié mon mot de passe</a></span><br><br>
+
+        <!-- TODO : remplacer l'identifiant par une classe partagée par tous les boutons submit des pages d'authentification -->
         <input type="submit" id="connexion__bouton__connecter" name="se_connecter" value="Se connecter" title="Cliquez ici pour vous connecter"><br>
+
         <?php if (isset($message)) echo "<strong>$message</strong>";?>
+
         <hr>
         <p>Vous n'avez pas encore de compte ?</p>
+        <!-- TODO : Même chose que pour le bouton "J'ai oublié mon mot de passe" -->
         <span id="connexion_bouton_inscription"><a style="text-decoration:none" href="signup.php" title="Cliquez ici pour créer votre compte">Créer un compte</a></span>
       </form>
     </div>
