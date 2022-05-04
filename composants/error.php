015b7423dd35319b37d6462ad751f5878348a856
axllblc <59664317+axllblc@users.noreply.github.com>
2022-05-04 15:35:02 +0200

    Modification des pages d'erreur (suite)

---
+++ composants/error.php
@@ -0,0 +1,34 @@
+<?php
+
+/* ⚠️ Page d'erreur */
+
+require_once '../composants/html_head.php';
+
+/**
+ * Afficher une page d'erreur. L'appel de cette fonction met fin à l'exécution du script.
+ * @param int $status Code HTTP décrivant l'erreur (404 par exemple)
+ * @return void
+ */
+function error (int $status): void {
+  http_response_code($status);
+
+  ?>
+
+  <!DOCTYPE html>
+  <html lang="fr">
+  <?php htmlHead(); ?>
+  <body>
+    <p>
+      <strong>Erreur <?php echo $status ?></strong>
+      <?php
+        if ($status === 403) echo ' : accès refusé.';
+        if ($status === 404) echo ' : la page demandée est introuvable. Elle a peut-être été déplacée ou supprimée.';
+      ?>
+    </p>
+
+    <a href="/avocaba">Revenir à l'accueil</a>
+  </body>
+  </html>
+
+  <?php exit;
+}
