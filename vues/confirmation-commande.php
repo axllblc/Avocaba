539d0cc078eed106322e227d9b5fcaffd3706782
axllblc <59664317+axllblc@users.noreply.github.com>
2022-05-08 18:57:25 +0200

    Amélioration de l'apparence de la page confirmation-commande

--- vues/confirmation-commande.php
+++ vues/confirmation-commande.php
@@ -1,13 +1,13 @@
 <?php
 
-/* Page de confirmation de commande */
+/* ✅ Page de confirmation de commande */
 
 require_once $_SERVER['DOCUMENT_ROOT'] . '/avocaba/traitements/misc.inc.php';
 require_once $_SERVER['DOCUMENT_ROOT'] . '/avocaba/composants/html_head.php';
 require_once $_SERVER['DOCUMENT_ROOT'] . '/avocaba/composants/html_header.php';
 require_once $_SERVER['DOCUMENT_ROOT'] . '/avocaba/composants/footer.php';
 
-// On affiche cette page que si on vient d'effectuer une commande
+// On n'affiche cette page que si l'on vient d'effectuer une commande
 if(!isset($_GET['no'])){
   header('Location: espace-client/account.php');
 }
@@ -21,9 +21,12 @@ if(!isset($_GET['no'])){
     <?php htmlHeader() ?>
     <main>
       <h1>Commande validée</h1>
-      <p>Numéro de commande : <span><?php if(isset($_GET['no'])) echo $_GET['no'] ?></span></p>
-      <p>Félicitations, votre commande a bien été validée par Avocaba. Vous allez recevoir un email vous précisant les conditions de retrait de votre commande</p>
-      <a id="commande__bouton-commande" href="espace-client/account.php?btClient=commandes">Accéder à mes commandes</a>
+      <p>Numéro de commande : <b><?php if(isset($_GET['no'])) echo $_GET['no'] ?></b></p>
+      <p>
+        Votre commande a bien été validée par Avocaba&nbsp! Merci pour votre confiance. Vous allez recevoir un email
+        vous précisant les conditions de retrait de votre commande.
+      </p>
+      <a id="commande__bouton-commande" class="btn btn--filled" href="espace-client/account.php?btClient=commandes">Accéder à mes commandes</a>
     </main>
     <?php footer() ?>
   </body>
