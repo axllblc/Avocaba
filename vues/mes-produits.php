bbd3a07c8d5610925d4c3c8387122f7bc180f80a
Baratinus <71899617+Baratinus@users.noreply.github.com>
2022-05-08 17:54:11 +0200

    style mes-produits

--- vues/mes-produits.php
+++ vues/mes-produits.php
@@ -25,6 +25,10 @@ if(!isset($_SESSION['Client'])){
   <body>
     <?php htmlHeader(!empty($_SESSION['Depot'])); ?>
 
+    <style>
+      
+    </style>
+
     <main>
       <h1>Mes Produits</h1>
 
@@ -36,22 +40,30 @@ if(!isset($_SESSION['Client'])){
       <?php
       } else {
         ?>
-        <ul>
-          <?php foreach ($articles_favoris as $key => $value) { ?>
-          <li>
-            <img src="<?php echo $value['PhotoVignette']; ?>" 
-                 alt="image de vignette de <?php echo $value['NomArticle']; ?>">
-            <div class="mes-produits__descriptif">
-              <h2><a href="/avocaba/vues/article.php?IdArticle=<?php echo $value['IdArticle']; ?>"><?php echo $value['NomArticle']; ?></a></h2>
-              <p><a href="/avocaba/vues/fournisseur.php?siret=<?php echo $value['SiretProducteur']; ?>" class="mes-produits__nom-producteur"><?php echo $value['NomProducteur']; ?></a></p>
-              <p><?php echo $value['Prix']; ?></p>
-            </div>
-            <a href="/avocaba/traitements/favoris-actions.php?table=articles&id=<?php echo $value['IdArticle']; ?>">
-              <span class="material-icons">delete</span>
-            </a>
-          </li>
-          <?php } ?>
-        </ul>
+        <table class="mes-produits__tableaux">
+          <tbody>
+            <?php foreach ($articles_favoris as $key => $value) { ?>
+            <tr class="mes-produits__produit">
+              <td class="mes-produits__cellule-image">
+                <img src="<?= !empty($value['PhotoVignette']) ? $value['PhotoVignette'] : '/avocaba/img/article-placeholder.png' ?>" 
+                    alt="image de vignette de <?php echo $value['NomArticle']; ?>">
+              </td>
+
+              <td class="mes-produits__descriptif">
+                <h2><a href="/avocaba/vues/article.php?IdArticle=<?php echo $value['IdArticle']; ?>"><?php echo $value['NomArticle']; ?></a></h2>
+                <p><a href="/avocaba/vues/fournisseur.php?siret=<?php echo $value['SiretProducteur']; ?>" class="mes-produits__nom-producteur"><?php echo $value['NomProducteur']; ?></a></p>
+                <p><?php echo $value['Prix']; ?> €</p>
+              </td>
+
+              <td>
+                <a href="/avocaba/traitements/favoris-actions.php?table=articles&id=<?php echo $value['IdArticle']; ?>">
+                  <span class="material-icons mes-produit__delete">delete</span>
+                </a>
+              </td>
+            </tr>
+            <?php } ?>
+          </tbody>
+        </table>
         <?php } ?>
     </main>
 
