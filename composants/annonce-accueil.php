59043e358bc18ec69cb8c4ed1e9644c20422fbe9
axiolerre <39837270+axiolerre@users.noreply.github.com>
2022-05-08 20:01:22 +0200

    magasin articles producteur uniquement dispo

--- composants/annonce-accueil.php
+++ composants/annonce-accueil.php
@@ -9,7 +9,7 @@ require_once $_SERVER['DOCUMENT_ROOT'] . '/avocaba/traitements/articles.inc.php'
  * Composant annonce sur la page d'accueil
  * @param string|int $depot Identifiant du dépôt actif du client
  */
-function annonceAccueil(string|int $depot) : void { 
+function annonceAccueil(string|int $depot) : void {
   // récupération des fournisseurs sur le dépôt du client
   $fournisseurs = Fournisseur::fournisseurSurDepot($depot);
 
@@ -24,12 +24,12 @@ function annonceAccueil(string|int $depot) : void {
   ?>
 <div class="annonce-accueil">
   <div class="annonce-accueil__slideshow-container">
-    <?php 
+    <?php
     $photos = $fournisseur->photoFournisseur();
     foreach ($photos as $key => $value) {
     ?>
     <div class="annonce-accueil__mySlides annonce-accueil__fade">
-      <img src="<?php echo $value; ?>" 
+      <img src="<?php echo $value; ?>"
            alt="photo <?php echo $fournisseur->getNom(); echo ' '.$key; ?>">
     </div>
     <?php } ?>
@@ -38,38 +38,45 @@ function annonceAccueil(string|int $depot) : void {
   <script>
     let slideIndex = 0;
     showSlides();
-    
+
     function showSlides() {
       let i;
       let slides = document.getElementsByClassName("annonce-accueil__mySlides");
       for (i = 0; i < slides.length; i++) {
-        slides[i].style.display = "none";  
+        slides[i].style.display = "none";
       }
       slideIndex++;
-      if (slideIndex > slides.length) {slideIndex = 1}    
-      
-      slides[slideIndex-1].style.display = "block";  
+      if (slideIndex > slides.length) {slideIndex = 1}
+
+      slides[slideIndex-1].style.display = "block";
       setTimeout(showSlides, 4000);
     }
   </script>
-  
+
   <div class="annonce-accueil__droite">
     <h2 class="annonce-accueil__decouvrir">Venez découvrir <?php echo $fournisseur->getNom(); ?></h2>
     <p class="annonce-accueil__produits-phares">
       Découvrez les produits proposés par <b><?php echo $fournisseur->getNom(); ?></b>&nbsp;:
-      <?php 
+      <?php
       $produits_phares = $fournisseur->produitsPhares();
       $i = 0;
       while ($i < count($produits_phares)-1) {
-        $produit = rechercherArticle($produits_phares[$i], 'idArticle')[0];
-      ?>
-      <a href="/avocaba/vues/article.php?IdArticle=<?php echo $produit['IdArticle']; ?>"><?php echo $produit['Nom']; ?></a>, 
-      <?php 
-        $i++; 
-      } 
-      $produit = rechercherArticle($produits_phares[$i], 'idArticle')[0];
+        $produit = rechercherArticle($produits_phares[$i], 'idArticle', $depot);
+        if($produit != null){
+          $produit = $produit[0];
+
       ?>
       <a href="/avocaba/vues/article.php?IdArticle=<?php echo $produit['IdArticle']; ?>"><?php echo $produit['Nom']; ?></a>,
+      <?php
+        }
+        $i++;
+      }
+      $produit = rechercherArticle($produits_phares[$i], 'idArticle', $depot);
+      if($produit != null){
+        $produit = $produit[0];
+      ?>
+      <a href="/avocaba/vues/article.php?IdArticle=<?php echo $produit['IdArticle']; ?>"><?php echo $produit['Nom']; ?></a>,
+      <?php } ?>
       et bien plus encore...
     </p>
   </div>
